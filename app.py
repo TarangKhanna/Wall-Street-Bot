c03a3dd55933f42111f4f70b14cd86bb40cfeece
Tarang Khanna <tarangalbert@gmail.com>
2017-01-13 09:20:43 -0500

    x

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ from twitter_analyze import twitter_analyze
 from yahoo_finance import Share
 from datetime import datetime, timedelta
 import requests
-import mysql.connector
+# import mysql.connector
 
 app = Flask(__name__)
 
-cnx = mysql.connector.connect(user=os.environ['JW_USERNAME'], password=os.environ['JW_KEY'], host=os.environ['JW_HOST'], database='xcqk05aruwtw0kew')
+# cnx = mysql.connector.connect(user=os.environ['JW_USERNAME'], password=os.environ['JW_KEY'], host=os.environ['JW_HOST'], database='xcqk05aruwtw0kew')
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -235,16 +235,16 @@ def extract_days(time):
 # intent current price
 def getStockCurrentPrice(req):
     # db test
-    print 'Accessing database'
-    cursor = cnx.cursor(buffered = True)
-    str_call = 'SELECT * FROM USER_BASIC_INFO'
-    cursor.execute(str_call);
+    # print 'Accessing database'
+    # cursor = cnx.cursor(buffered = True)
+    # str_call = 'SELECT * FROM USER_BASIC_INFO'
+    # cursor.execute(str_call);
 
-    data = cursor.fetchone()
-    cnx.commit()
-    cursor.close()
+    # data = cursor.fetchone()
+    # cnx.commit()
+    # cursor.close()
 
-    print data[0]
+    # print data
     # db test
 
     result = req.get("result")
